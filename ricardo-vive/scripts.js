83d57e433d6a214d863937f198c0dcd90e11ba10
Lautaro Bermejo <tarolau97@hotmail.com>
2018-03-23 20:20:12 -0300

    Ricardo vive en la gente!

---
+++ ricardo-vive/scripts.js
@@ -0,0 +1,22 @@
+let buttons = document.querySelectorAll(".button");
+let sounds = document.querySelectorAll("audio");
+
+function playAudio(event){
+  let audio = event.target.parentElement.getElementsByTagName("audio")[0];
+  audio.play();
+}
+
+buttons.forEach(btn => btn.addEventListener("click", playAudio));
+
+
+//Silenciar todos los sonidos
+window.addEventListener("keydown", function(e){
+  console.log(e.keyCode);
+  if(e.keyCode == 27){ //Si presiona la tecla ESC
+    console.log("Funcionando");
+    sounds.forEach(function(sound){
+      sound.pause();
+      sound.currentTime = 0;
+    });
+  }
+});
