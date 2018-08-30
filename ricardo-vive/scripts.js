970bfd1c79decbc6788420dd564c0cdd5b115c13
bermejolautaro <tarolau97@hotmail.com>
2018-08-29 23:15:10 -0300

    Add files via upload

--- ricardo-vive/scripts.js
+++ ricardo-vive/scripts.js
@@ -1,15 +1,43 @@
+// Necesario contar con conocimiento de al menos uno de estos lenguajes:
+//
+//     Javascript
+//     Java
+//     GO
+//     Python
+//     HTML
+//
+// Experiencia deseable (no excluyente) con:
+//
+//     Google Analytics
+//     Google Tag Manager
+//     Firebase Analytics
+//     jQuery
+//     R
+//     Google Cloud (o similares: Amazon Web Services, Microsoft Azure, Oracle Cloudera)
+
+
 let buttons = document.querySelectorAll(".button");
 let sounds = document.querySelectorAll("audio");
 
+//Defino la funcion playAudio que dice como debe ser reproducido un audio
 function playAudio(event){
   let audio = event.target.parentElement.getElementsByTagName("audio")[0];
   sounds.forEach(function(sound){
     sound.pause();
     sound.currentTime = 0;
   });
+
+  //Envio informacion sobre el boton a Google Analytics
+  gtag('event', 'clickBoton',{
+    'event_category' : 'botonesDeAudio',
+    'event_label' : 'Reproducir',
+    'value' : audio.src
+  });
+
   audio.play();
 }
 
+//Le agrega a todos los botones un evento CLICK que llama al meto playAudio
 buttons.forEach(btn => btn.addEventListener("click", playAudio));
 
 
